76c2ac7f5bbdc77698a5a757a570e2f8e0a52e40
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-24 15:26:13 -0700

    Fully functioning pig game, WIP CSS
    Co-authored by:  Samuel Majerus <ladolego@gmail.com>
    Co-authored-by: Tiberius Lockett <tlockett680@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 /*   Business Logic   */
 Player.prototype.diceRoll = function() {
   const min = Math.ceil(1);  
-  let number = Math.floor((Math.random() * 6) + min);  //Both max and min are inclusive. [See last example on MDN's 'Math.random()' page for reference. -SM]
-  // let numInner = (Math.random() * 6) + 1;
-  // console.log("(Math.random * 6)+1 = " + numInner); 
-  // console.log("Applying 'Math.floor' on 'numInner', we get:  " + Math.floor(numInner)); 
+  let number = Math.floor((Math.random() * 6) + min);  //Both max and min are inclusive. [See last example on MDN's 'Math.random()' page for reference. -SM] 
   if (number !== 1) { 
     this.currentTotal.push(number);
   } else {  //(If 'number === 1')
+    console.log("Resetting currentTotal");
+    console.log(this.currentTotal);
+    game.changeTurn();
     return this.currentTotal = [0];
   }
-  console.log(this.currentTotal);
 }
 
 Player.prototype.currentSum = function() {
@@ -39,12 +38,33 @@ function Player(name, variable) {
   this.totalPts = variable; //Initializes this field to start at 0. 
 } 
 
+function Game(player1, player2) {
+  this.firstPlayer = player1
+  this.secondPlayer = player2
+  this.activePlayer = 1;
+}
+
+Game.prototype.changeTurn = function() {
+  if (this.activePlayer === 1) {
+    this.activePlayer = 2;
+    console.log("p1 to p2");
+    document.querySelector("div#p1Buttons").setAttribute("class", "hidden");
+    document.querySelector("div#p2Buttons").removeAttribute("class");
+  } else {
+    console.log("p2 to p1");
+    document.querySelector("div#p2Buttons").setAttribute("class", "hidden");
+    document.querySelector("div#p1Buttons").removeAttribute("class");
+    this.activePlayer = 1;
+  } 
+}
+
 Player.prototype.winner = function() {
   document.querySelector("#youWin").removeAttribute("class");
   document.querySelector("#gameboard").setAttribute("class", "hidden");
   document.querySelector("#winner").innerText = this.name;
 }
 
+let game;
 
 /*   UI Logic   */
 
@@ -54,33 +74,31 @@ function handleGameStart(event) {
   const inputPlayer2 = document.querySelector("input#player2").value;
   let player1 = new Player(inputPlayer1, 0);
   let player2 = new Player(inputPlayer2, 0);
+  game = new Game(player1, player2); 
   document.querySelector("div#game").removeAttribute("class");
   document.querySelector("span#player1").innerText = player1.name;
   document.querySelector("span#player2").innerText = player2.name;
   document.querySelector("div#p1Buttons").removeAttribute("class");
   document.getElementById("roll1").addEventListener("click",function() {
-    player1.diceRoll();
+    player1.diceRoll(); 
     document.querySelector("span#currpts1").innerHTML = player1.currentSum();
   });
   document.getElementById("roll2").addEventListener("click",function() {
-    player2.diceRoll();
+    player2.diceRoll(); 
     document.querySelector("span#currpts2").innerHTML = player2.currentSum();
   });
   document.querySelector("#hold1").addEventListener("click", function(){
     player1.sum();
     document.querySelector("span#totalpts1").innerHTML = player1.totalPts;
-    document.querySelector("div#p1Buttons").setAttribute("class", "hidden");
-    document.querySelector("div#p2Buttons").removeAttribute("class");
+    game.changeTurn(); 
   });
   document.querySelector("#hold2").addEventListener("click", function(){
     player2.sum();
     document.querySelector("span#totalpts2").innerHTML = player2.totalPts;
-    document.querySelector("div#p2Buttons").setAttribute("class", "hidden");
-    document.querySelector("div#p1Buttons").removeAttribute("class");
+    game.changeTurn(); 
   });
 }
 
 window.addEventListener("load", function() {
   document.querySelector("form#nameInput").addEventListener("submit", handleGameStart);
-  // document.querySelector("form#gameboard").addEventListener("submit", handleGameRoll);
 }); 
