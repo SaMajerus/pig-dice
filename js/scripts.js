85ccd4aec3c89ea00ca165aa7d585694f262d644
Samuel Majerus <ladolego@gmail.com>
2022-08-24 12:17:47 -0700

    'Player' -- Test 1 passed.   Summary of changes: Constructor 'Player' added.  New function prototypes added accordingly.  Added Forms for user inputs, score tracking.  [WIP: UI Logic, player-turn var(s)]
    Co-authored by: Tiberius Lockett <tlockett680@gmail.com>
    Co-authored by: William Mentzer <william.mentzer2k@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,46 @@
-let currentTotal = [];
-let totalPoints = 0;
-
-function diceRoll() {
+/*   Business Logic   */
+Player.prototype.diceRoll = function() {
   let number = Math.floor((Math.random() * 6) + 1); 
   // let numInner = (Math.random() * 6) + 1;
   // console.log("(Math.random * 6)+1 = " + numInner); 
   // console.log("Applying 'Math.floor' on 'numInner', we get:  " + Math.floor(numInner)); 
-  if (number !== 1) {
-    currentTotal.push(number);
+  if (number !== 1) { 
+    this.currentTotal.push(number);
   } else if (number === 1) {
     return currentTotal = [];
   }
-  console.log(currentTotal);
+  console.log(this.currentTotal);
 }
 
-function sum(currentTotal) {  //(Local-var 'totalPoints' modified to 'totalPts' for revision/clarification of Test 1.)
-  let totalPts = totalPoints;
-  for (let i = 0; i < currentTotal.length; i++) {
-    totalPts += currentTotal[i];
+Player.prototype.currentSum = function() {
+  this.currentPoints = 0;
+  for (let i=0; i < this.currentTotal.length; i++) {
+    this.currentPoints += this.currentTotal[i];
+  } if (this.totalPts >= 100) {
+    this.winner();
   }
-  return totalPts;
+  return this.currentPoints;
 }
 
-function Player(name, totalPoints) {
+Player.prototype.sum = function() {  //(Local-var 'totalPoints' modified to 'totalPts' for revision/clarification of Test 1.)
+ this.totalPts = 0;
+  for (let i = 0; i < this.currentTotal.length; i++) {
+    this.totalPts += this.currentTotal[i];
+  }
+  return this.totalPts;
+}
+
+function Player(name, variable) {
   this.name = name;
   this.currentTotal = [];
-  this.totalPoints = 0; //Initializes this field to start at 0. 
+  this.totalPts = variable; //Initializes this field to start at 0. 
 } 
 
+Player.prototype.winner = function() {
+  document.querySelector("#youWin").removeAttribute("class");
+  document.querySelector("#gameboard").setAttribute("class", "hidden");
+  document.querySelector("#winner").innerText = this.name;
+
+}
+
+/*   UI Logic   */
