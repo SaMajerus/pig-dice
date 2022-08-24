37a657a46a1bf42901346d3b8d0b46d8078de406
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-24 14:08:48 -0700

    add main page game functionality
    Co-authored by:  Samuel Majerus <ladolego@gmail.com>
    Co-authored-by: Tiberius Lockett <tlockett680@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 /*   Business Logic   */
 Player.prototype.diceRoll = function() {
-  let number = Math.floor((Math.random() * 6) + 1); 
+  const min = Math.ceil(1);  
+  let number = Math.floor((Math.random() * 6) + min);  //Both max and min are inclusive. [See last example on MDN's 'Math.random()' page for reference. -SM]
   // let numInner = (Math.random() * 6) + 1;
   // console.log("(Math.random * 6)+1 = " + numInner); 
   // console.log("Applying 'Math.floor' on 'numInner', we get:  " + Math.floor(numInner)); 
   if (number !== 1) { 
     this.currentTotal.push(number);
   } else if (number === 1) {
-    return currentTotal = [];
+    return this.currentTotal = [0];
   }
   console.log(this.currentTotal);
 }
@@ -44,3 +45,36 @@ Player.prototype.winner = function() {
 }
 
 /*   UI Logic   */
+
+function handleGameStart(event) {
+  event.preventDefault();
+  const inputPlayer1 = document.querySelector("input#player1").value;
+  const inputPlayer2 = document.querySelector("input#player2").value;
+  let player1 = new Player(inputPlayer1, 0);
+  let player2 = new Player(inputPlayer2, 0);
+  document.querySelector("div#game").removeAttribute("class");
+  document.querySelector("span#player1").innerText = player1.name;
+  document.querySelector("span#player2").innerText = player2.name;
+  document.getElementById("roll1").addEventListener("click",function() {
+    player1.diceRoll();
+    document.querySelector("span#currpts1").innerHTML = player1.currentTotal;
+  });
+  document.getElementById("roll2").addEventListener("click",function() {
+    player2.diceRoll();
+    document.querySelector("span#currpts2").innerHTML = player2.currentTotal;
+  });
+  document.querySelector("#hold1").addEventListener("click", function(){
+    player1.sum();
+    document.querySelector("span#totalpts1").innerHTML = player1.totalPts;
+  });
+  document.querySelector("#hold2").addEventListener("click", function(){
+    player2.sum();
+    document.querySelector("span#totalpts2").innerHTML = player2.totalPts;
+  });
+
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#nameInput").addEventListener("submit", handleGameStart)
+  // document.querySelector("form#gameboard").addEventListener("submit", handleGameRoll)
+}); 
